841eb50e80d5ee465ed32ee6e6cfd510b8da6e06
bruno bonomo <you@example.com>
2025-03-19 22:48:06 -0300

    Iniciamos un Tutorial de Git y Github

---
+++ roofPython.py
@@ -0,0 +1,21 @@
+# Quadratic Formula
+
+a = int(input("Ingresar a :"))
+b = int(input("Ingresar b :"))
+c = int(input("Ingresar c :"))
+
+root1 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+root2 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+
+print(root1)
+print(root2)
+
+
+# Pythagorean Theorem 📐
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+c = (a**2 + b**2) ** 0.5
+
+print(c)
